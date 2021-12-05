7c34b3cc57256c179b9d691db0dc69583f748f0b
PC <PC@DESKTOP-5AUI3JO>
2021-12-05 15:41:53 +0300

    Proje dosyası ekle.

---
+++ Metadlar2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Metadlar2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 30;
+            urun1.Aciklama = "Fransa Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Armut";
+            urun2.Fiyati = 40;
+            urun2.Aciklama = "Deveci Armudu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("......................");
+            }
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+                
+
+           
+
+           
+
+
+
+
+           
+
+           
+      
+           
+
+
+
+
+            
+            
+
+
+
+            
+        }
+    }
+}
---
+++ Metadlar2/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metadlar2
+{
+    internal class SepetManager
+    {
+       public void Ekle(Urun urun) 
+        {
+            Console.WriteLine("Sepete Eklediniz : " + urun.Adi);
+        }
+            
+
+          
+        
+
+    }
+}
